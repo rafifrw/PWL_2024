9930477db23e6ca134624a2ac164710486b722c3
rafifrw <rafifrw@gmail.com>
2024-08-28 14:19:42 +0700

    modifikasi tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,17 @@
 <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale())}}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-sclae=1">
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h3>Semangat Belajar Laravel 10</h3>
+    </body>
+</html>
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -130,4 +143,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
